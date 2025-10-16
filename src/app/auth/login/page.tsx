e35126757a982d2ec2bc9dd4a67eb7ca01816015
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-16 10:11:59 -0300

    feat: adds page.tsx to src/app/auth/auth/login

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,28 @@
+import { Container, Card, Heading, Flex, Text, Link } from "@radix-ui/themes";
+import SigninForm from "@/components/auth/SigninForm";
+import NavLink from "next/link";
+
+function LoginPage() {
+  return (
+    <Container size="1" height="100%" className="p-3 md:p-0">
+      <Flex className="h-[calc(100vh-10rem)] w-full items-center">
+        <Card className="w-full p-7">
+          <Heading>Sign In</Heading>
+
+          <SigninForm />
+
+          <Flex justify="between" my="4">
+            <Text>Don&apos;t have an Account?</Text>
+
+            <Link asChild>
+              <NavLink href="/auth/register" passHref>
+                Sign Up
+              </NavLink>
+            </Link>
+          </Flex>
+        </Card>
+      </Flex>
+    </Container>
+  );
+}
+export default LoginPage;
