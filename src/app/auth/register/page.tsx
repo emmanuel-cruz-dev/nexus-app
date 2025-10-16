42113191633a79f0ba01c4609d8e7c8f7c446b55
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-16 10:13:35 -0300

    feat: adds page.tsx to src/app/auth/register/

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,26 @@
+import { Container, Card, Heading, Flex, Text, Link } from "@radix-ui/themes";
+import NavLink from "next/link";
+
+function RegisterPage() {
+  return (
+    <Container size="1" height="100%" className="p-3 md:p-0">
+      <Flex className="h-[calc(100vh-10rem)] w-full items-center">
+        <Card className="w-full p-7">
+          <Heading>Sign Up</Heading>
+
+          <Flex justify="between" my="4">
+            <Text>Already have an Account?</Text>
+
+            <Link asChild>
+              <NavLink href="/auth/login" passHref>
+                Sign In
+              </NavLink>
+            </Link>
+          </Flex>
+        </Card>
+      </Flex>
+    </Container>
+  );
+}
+
+export default RegisterPage;
