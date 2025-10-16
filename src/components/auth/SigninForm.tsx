1527c7cfbfa1df8a7e143ae428c06dfadb70ae08
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-16 10:14:44 -0300

    feat: adds SigninForm.tsx to src/components/auth/

---
+++ src/components/auth/SigninForm.tsx
@@ -0,0 +1,107 @@
+"use client";
+import { Flex, TextField, Button, Text } from "@radix-ui/themes";
+import { EnvelopeClosedIcon, LockClosedIcon } from "@radix-ui/react-icons";
+import { useForm, Controller } from "react-hook-form";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+function SigninForm() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    values: {
+      email: "",
+      password: "",
+    },
+  });
+  const router = useRouter();
+
+  const onSubmit = handleSubmit(async (data) => {
+    console.log(data);
+    const res = await signIn("credentials", {
+      redirect: false,
+      email: data.email,
+      password: data.password,
+    });
+
+    if (!res?.ok) {
+      console.log(res);
+    }
+
+    router.push("/dashboard");
+  });
+
+  console.log(errors);
+
+  return (
+    <form onSubmit={onSubmit}>
+      <Flex direction="column" gap="2">
+        <label htmlFor="email">Email</label>
+        <Controller
+          name="email"
+          control={control}
+          rules={{
+            required: {
+              value: true,
+              message: "Email is required",
+            },
+          }}
+          render={({ field }) => (
+            <TextField.Root
+              type="email"
+              placeholder="email@domain.com"
+              autoFocus
+              {...field}
+            >
+              <TextField.Slot>
+                <EnvelopeClosedIcon height="16" width="16" />
+              </TextField.Slot>
+            </TextField.Root>
+          )}
+        />
+
+        {errors.email && (
+          <Text color="ruby" className="text-xs">
+            {errors.email.message}
+          </Text>
+        )}
+
+        <label htmlFor="password">Password</label>
+        <Controller
+          name="password"
+          control={control}
+          rules={{
+            required: {
+              message: "Password is required",
+              value: true,
+            },
+            minLength: {
+              message: "Password must be at least 6 characters",
+              value: 6,
+            },
+          }}
+          render={({ field }) => (
+            <TextField.Root type="password" placeholder="*******" {...field}>
+              <TextField.Slot>
+                <LockClosedIcon height="16" width="16" />
+              </TextField.Slot>
+            </TextField.Root>
+          )}
+        />
+
+        {errors.password && (
+          <Text color="ruby" className="text-xs">
+            {errors.password.message}
+          </Text>
+        )}
+
+        <Button type="submit" mt="4">
+          Sign In
+        </Button>
+      </Flex>
+    </form>
+  );
+}
+export default SigninForm;
