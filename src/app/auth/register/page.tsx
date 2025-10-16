2bd9e23fb02d66c3d2e0a9bce2ee74001a1efbaa
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-16 12:47:33 -0300

    feat: updates content and styles in src/app/auth/register/page.tsx

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,19 +1,22 @@
 import { Container, Card, Heading, Flex, Text, Link } from "@radix-ui/themes";
+import SignupForm from "@/components/auth/SignupForm";
 import NavLink from "next/link";
 
 function RegisterPage() {
   return (
     <Container size="1" height="100%" className="p-3 md:p-0">
-      <Flex className="h-[calc(100vh-10rem)] w-full items-center">
+      <Flex className="h-screen w-full items-center">
         <Card className="w-full p-7">
-          <Heading>Sign Up</Heading>
+          <Heading mb="4">Registrarse</Heading>
+
+          <SignupForm />
 
           <Flex justify="between" my="4">
-            <Text>Already have an Account?</Text>
+            <Text>¿Ya tienes una cuenta?</Text>
 
             <Link asChild>
               <NavLink href="/auth/login" passHref>
-                Sign In
+                Iniciar sesión
               </NavLink>
             </Link>
           </Flex>
