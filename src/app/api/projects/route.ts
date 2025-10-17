4830fcf0edc00469ca9a12cf8c076c7b99cacd5b
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-17 13:28:42 -0300

    feat: adds route.ts to src/app/api/projects/

---
+++ src/app/api/projects/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth/next";
+import prisma from "@/libs/prisma";
+import { authOptions } from "../auth/[...nextauth]/route";
+
+export async function POST(request: Request) {
+  const data = await request.json();
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  const newProject = await prisma.project.create({
+    data: {
+      title: data.title,
+      description: data.description,
+      user: {
+        connect: {
+          id: parseInt(session.user.id),
+        },
+      },
+    },
+  });
+
+  return NextResponse.json(newProject, {
+    status: 201,
+  });
+}
