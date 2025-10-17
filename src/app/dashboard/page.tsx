2680bd976fbcbd89fe92d65af88aab67f0977b8e
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-17 12:28:27 -0300

    feat: updates DashboardPage content in src/app/dashboard/page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,21 @@
+"use client";
+
+import { Container, Heading, Button } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
+
 function DashboardPage() {
-  return <div>DashboardPage</div>;
+  const router = useRouter();
+
+  return (
+    <Container className="mt-10">
+      <div className="flex justify-between">
+        <Heading>Projects</Heading>
+        <Button onClick={() => router.push("/dashboard/projects/new")}>
+          Create Project
+        </Button>
+      </div>
+    </Container>
+  );
 }
 
 export default DashboardPage;
