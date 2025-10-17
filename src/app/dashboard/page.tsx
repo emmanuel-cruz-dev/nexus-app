e500e0081c8d30b05f12cf9ff4819d3f3211b4fd
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-17 15:54:22 -0300

    feat: adds loadProjects function and ProjectCard component to src/app/dashboard/page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,32 @@
-"use client";
+import HeaderDashboard from "@/components/dashboard/HeaderDashboard";
+import { Container, Grid } from "@radix-ui/themes";
+import prisma from "@/libs/prisma";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import ProjectCard from "@/components/projects/ProjectCard";
 
-import { Container, Heading, Button } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+async function loadProjects(userId: number) {
+  return await prisma.project.findMany({
+    where: {
+      userId,
+    },
+  });
+}
 
-function DashboardPage() {
-  const router = useRouter();
+async function DashboardPage() {
+  const session = await getServerSession(authOptions);
+  const userId = parseInt(session?.user.id as string);
+  const projects = await loadProjects(userId);
 
   return (
     <Container className="mt-10">
-      <div className="flex justify-between">
-        <Heading>Projects</Heading>
-        <Button onClick={() => router.push("/dashboard/projects/new")}>
-          Create Project
-        </Button>
-      </div>
+      <HeaderDashboard />
+
+      <Grid columns={{ initial: "1", sm: "2", md: "3" }} gap="4">
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </Grid>
     </Container>
   );
 }
