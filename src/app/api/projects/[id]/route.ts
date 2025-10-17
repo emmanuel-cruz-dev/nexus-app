73d267773fddbbe99ef15adfdafb933f11a77d20
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-17 17:09:02 -0300

    feat: adds sonner toast to ProjectDetailPage in  src/app/api/projects/[id]/route.ts

--- src/app/api/projects/[id]/route.ts
+++ src/app/api/projects/[id]/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import prisma from "@/libs/prisma";
+import { Prisma } from "../../../../../generated/prisma";
 
 export async function GET(
   request: Request,
@@ -20,4 +21,38 @@ export async function GET(
   }
 
   return NextResponse.json(project);
+}
+
+export async function DELETE(
+  request: Request,
+  {
+    params,
+  }: {
+    params: { id: string };
+  }
+) {
+  try {
+    const projectDeleted = await prisma.project.delete({
+      where: {
+        id: parseInt(params.id),
+      },
+    });
+
+    console.log("Project deleted successfully");
+    return NextResponse.json(projectDeleted, { status: 200 });
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2025") {
+        return NextResponse.json(
+          { error: "Project not found" },
+          { status: 404 }
+        );
+      }
+    }
+
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
 }
